e70cb0526074ea35eec094373241abf5146d746d
patrzykont <aaronpatrzykont@Aarons-MacBook-Air.local>
2018-01-20 08:26:00 -0600

    new

---
+++ project2/dogs.js
@@ -0,0 +1,87 @@
+//dogs
+var dogArray=[
+
+{
+		name = 'Buster'
+		photo =
+		description = ''
+		number = 1
+
+},
+
+{
+		name = 'Jimbo'
+		photo =
+		description = ''
+		number = 2
+
+
+},
+
+{
+		name = 'Misty'
+		photo =
+		description = ''
+		number = 3
+
+},
+
+{
+		name = 'Savanah'
+		photo =
+		description = ''
+		number = 4
+
+},
+
+{
+		name = 'Blade'
+		photo =
+		description = ''
+		number = 5
+
+},
+
+{
+		name = 'Carrington'
+		photo =
+		description = ''
+		number = 6
+
+},
+
+{
+		name = 'Ronnie'
+		photo =
+		description = ''
+		number = 7
+
+},
+
+{
+		name = 'Barbara'
+		photo =
+		description = ''
+		number = 8
+
+},
+
+{
+		name = 'Fluffy'
+		photo =
+		description = ''
+		number = 9
+
+},
+
+{
+		name = 'Dirk-Nasty'
+		photo =
+		description = ''
+		number = 11
+
+},
+
+
+];
+module.exports = dogsArray;
